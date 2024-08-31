eb5f4c21cbad8801565f1dc5fa9df76051e44718
ashu08-code <ashu9908kumar@gmail.com>
2024-08-31 23:08:55 +0530

    initial commit

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,112 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc skills_flexlist pl-2">
+        <li>React.js</li>
+        <li>Next.js</li>
+        <li>Node.js</li>
+        <li>Express.js</li>
+        <li>JavaScript</li>
+        <li>Html</li>
+        <li>CSS</li>
+        <li>SCSS</li>
+        <li>Bootstrap</li>
+        <li>Material UI</li>
+        <li>Tailwind CSS</li>
+        <li>Redux Toolkit</li>
+        <li>MongoDb</li>
+        <li>Basic Git & Github</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li> B.Tech
+ Shivajirao Kadam Institute of Technology and Management • Indore
+ I completed my graduation in 2024 with 7.9 CGPA .</li>
+        <li>University of California, Santa Cruz</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>AWS Cloud Practitioner</li>
+        <li>Google Professional Cloud Developer</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            {/* I am a full stack web developer with a passion for creating
+            interactive and responsive web applications. I have experience
+            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
+            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
+            looking to expand my knowledge and skill set. I am a team player and
+            I am excited to work with others to create amazing applications. */}
+             I am a MERN Stack Developer with a proven track record of creating efficient, scalable applications. Skilled in front-end
+ and back-end technologies. I am adept in frontend designing and API Integration. And I want to constantly upgrade my
+ knowledge and skills and use them for the benefits and growth of the organization along with myself.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            {/* <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton> */}
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/app/components/Education.jsx
@@ -0,0 +1,36 @@
+"use client";
+import React from 'react'
+
+const Education = () => {
+  return (
+    <section className="text-white">
+        <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        Education
+      </h2>
+      <p className="text-base lg:text-lg mb-2">
+      • Bachelor of Technology
+        </p>
+        {/* <p> Shivajirao Kadam Institute of Technology and Management • Indore
+        I completed my graduation in 2024 with 7.9 CGPA </p> */}
+        <p className="text-[#ADB7BE] text-base sm:text-md mb-1 lg:text-lg">
+        Shivajirao Kadam Institute of Technology and Management, Indore
+          </p>
+          <p className="text-[#ADB7BE] text-base sm:text-md mb-6 lg:text-lg">I completed my graduation in 2024 with 7.9 CGPA</p>
+
+
+
+          <p className="text-base lg:text-lg mb-2">
+      •  Higher Secondary Education
+        </p>
+        <p className="text-[#ADB7BE] text-base sm:text-md mb-1 lg:text-lg">
+        La Sagesse Academy, Indore
+          </p>
+          <p className="text-[#ADB7BE] text-base sm:text-md mb-8 lg:text-lg">
+          I completed 12th standard with 77% in the year of 2020 </p>
+
+
+    </section>
+  ) 
+}
+
+export default Education
